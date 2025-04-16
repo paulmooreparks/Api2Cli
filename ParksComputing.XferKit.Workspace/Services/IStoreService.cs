a7feacaf72c58294951888ea2270708f12de25c6
Paul Parks <paulp@agilebridge.co.za>
2025-04-16 15:46:34 +0800

    Split XferKit into its own repo.

---
+++ ParksComputing.XferKit.Workspace/Services/IStoreService.cs
@@ -0,0 +1,9 @@
+﻿namespace ParksComputing.XferKit.Workspace.Services;
+
+public interface IStoreService : IDictionary<string, object>
+{
+    void ClearStore();
+    void Delete(string key);
+    object? Get(string key);
+    void Set(string key, object value);
+}
