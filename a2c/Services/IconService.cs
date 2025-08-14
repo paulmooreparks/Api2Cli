0133154ce7407b6e83baf7a7f603c8375ca83380
Paul Parks <paul@parkscomputing.om>
2025-08-14 15:56:25 +0800

    Another build fix.

--- a2c/Services/IconService.cs
+++ a2c/Services/IconService.cs
@@ -32,26 +32,18 @@ public static class IconService
         stream.Read(buffer, 0, buffer.Length);
         return buffer;
     }
-    
+
     /// <summary>
     /// Gets the path to the application icon file for the current platform
     /// </summary>
     /// <returns>Icon file path or null</returns>
-    public static string? GetIconPath()
-    {
+    public static string? GetIconPath() {
         var platform = Environment.OSVersion.Platform;
         var appDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        
-        return platform switch
-        {
+
+        return platform switch {
             PlatformID.Win32NT => Path.Combine(appDirectory!, "a2c.ico"),
-        if (appDirectory == null)
-            return null;
-        
-        return platform switch
-        {
-            PlatformID.Win32NT => Path.Combine(appDirectory, "a2c.ico"),
-            PlatformID.Unix => Path.Combine(appDirectory, "a2c.png"),
+            PlatformID.Unix => Path.Combine(appDirectory!, "a2c.png"),
             _ => null
         };
     }
